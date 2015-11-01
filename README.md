2f2d55f5fd0265c43f0f18cd5391b1db4005cb02
RatedRion <ratedrion@gmail.com>
2015-11-01 06:34:29 -0500

    Spacing fixed, comments, README!

---
+++ README.md
@@ -0,0 +1,25 @@
+I'm Feeling Happy!
+========
+Ever wanted to be so optimistic you wanted to see the most positive link from Google? 
+
+Ever so happy about the link you didn't care whether it was relevant to your search or not, as long as it was a site full of smiles?
+
+WORRY NO MORE FRIEND, for this is the I'm Feeling Happy button for Google.
+
+Type whatever you want anywhere you want (on the search bar), and press the I'm Feeling Happy button to be whisked away into a wonderland of sunshine and rainbows.
+
+<sub>Happiness rating not guaranteed. Seek help if you experience any side-effects.</sub>
+
+API's Used
+-------
+- HP HavenOnDemand
+- Google Custom Search
+
+Made By
+-------
+- Jia Sen Wu
+- Alvin Leung
+- Haoxin Luo
+- Brandon Ip
+
+Created for BostonHacks Fall 2015.
--- js/content.js
+++ js/content.js
@@ -1,92 +1,87 @@
 var runSentimentSearch = function(){
-    var sentimental = -1;
-    var sentiUrl = "http://google.com/";
+    var sentimental = -1; // Sentimental lowest is -1
+    var sentiUrl = "http://google.com/"; // Default url in case of fuckups
     var loop;
     
     var xmlhttp = new XMLHttpRequest();
-    var query = document.getElementById("lst-ib").value;
-		var googlekey = "";
-		var havenkey = "";
+    var query = document.getElementById("lst-ib").value; // Search bar for google
+    var googlekey = "AIzaSyD_14Ih1GGAUYqOiX-_dlHZfCli4MYLBVk"; // Your Google API key
+    var havenkey = "f8003a4c-8955-4b29-b21b-ae353c626758"; // Your HavenOnDemand API key
     var url = "https://www.googleapis.com/customsearch/v1?key=" + googlekey + "&cx=002494473691101639048:kfwgrseieb0&q=" + query;
-    xmlhttp.onreadystatechange = function() { // async
-	if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-	    var myArr = JSON.parse(xmlhttp.responseText);
-	    parseSearch(myArr);
-	}
+    xmlhttp.onreadystatechange = function() { // Retrieving JSON data
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var parseJSON = JSON.parse(xmlhttp.responseText);
+            parseSearch(parseJSON);
+        }
     }
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 
-    //parseSearch = function(arr,callback){
-    function parseSearch(arr) {
-	var urls = [];
-	console.log
-	if( arr["items"] === undefined ) {
-		window.location.replace("https://www.google.com/#q=" + query);
-	} else {
-		loop = arr["items"].length;
-		for(i = 0; i < arr["items"].length; i++) {
-				urls.push(arr["items"][i].link);
-		}
-		for(i = 0; i < urls.length; i++ ) { // Runs 10x
-				sentiment(urls[i]);
-		}
-	}
+    function parseSearch(JSON) {
+        if (JSON["items"] === undefined) { // If there are zero search results, returns regular google search 
+            window.location.replace("https://www.google.com/#q=" + query);
+        } else {
+            loop = JSON["items"].length;
+            for (i = 0; i < JSON["items"].length; i++) {
+                sentiment(JSON["items"][i].link); // Run through HavenOnDemand API
+            }
+        }
     };
-		
 
     function sentiment(data) {
-	var xmlhttp = new XMLHttpRequest();
-	var url = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?apikey=" + havenkey + "&url=" + data;
-	xmlhttp.onreadystatechange = function() { // async
-	    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-		loop--;
-		var myArr = JSON.parse(xmlhttp.responseText);
-		//console.log(myArr["aggregate"].score);
-		//console.log(loop);
-		if( myArr["aggregate"].score > sentimental ) {
-		    sentimental = myArr["aggregate"].score;
-		    sentiUrl = data;
-		}
-	    } else if (xmlhttp.readyState == 4 ) {
-		loop--;
-	    }
-	    if( loop == 0 ) {
-		window.location.replace(sentiUrl);
-	    }
-	}
-	xmlhttp.open("GET", url, true);
-	xmlhttp.send();
+        var xmlhttp = new XMLHttpRequest();
+        var url = "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1?apikey=" + havenkey + "&url=" + data;
+        xmlhttp.onreadystatechange = function() { // Retrieving JSON data
+            if (xmlhttp.readyState == 4) {
+                //console.log(loop);
+                loop--;
+                if (xmlhttp.status == 200) { // Run on success
+                    //console.log(myArr["aggregate"].score);
+                    var myArr = JSON.parse(xmlhttp.responseText);
+                    if (myArr["aggregate"].score > sentimental) { // Only replaces if score is higher for most positive search
+                        sentimental = myArr["aggregate"].score;
+                        sentiUrl = data;
+                    }
+                }
+            }
+            if (!loop) { // When done going through all searches, redirects to the happiest url
+                window.location.replace(sentiUrl);
+            }
+        }
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
     }
 }
 
 var addButton = function() {
-    document.getElementsByClassName("sbsb_g")[0].appendChild(outerSpan);
+    if (document.getElementsByClassName("sbsb_g")[0] != undefined) { // Cannot append to undefined
+        document.getElementsByClassName("sbsb_g")[0].appendChild(outerSpan);
+    }
 };
 
-
-//Creating Elements
-var btn = document.createElement("input");
-btn.setAttribute("value", "I'm Feeling Happy");
+var btn = document.createElement("input"); // Body button
+btn.value = "I'm Feeling Happy";
+btn.type = "submit";
 btn.setAttribute("aria-label", "I'm Feeling Happy");
-btn.setAttribute("type", "submit");
-btn.addEventListener("click", function(event){
+btn.addEventListener("click", function(event) {
     event.preventDefault();
     runSentimentSearch();
-});
-//Appending to DOM 
+}); 
 document.getElementsByTagName("center")[0].appendChild(btn);
 
-
-var sBtn = document.createElement("input");
-sBtn.setAttribute("type", "button");
-sBtn.setAttribute("value", "I'm Feeling Happy");
+var sBtn = document.createElement("input"); // Search window button
+sBtn.type = "button";
+sBtn.value = "I'm Feeling Happy";
 sBtn.setAttribute("class", "lsb");
 sBtn.addEventListener("mousedown", runSentimentSearch);
+
 var innerSpan = document.createElement("span");
 innerSpan.setAttribute("class", "lsbb");
+
 var outerSpan = document.createElement("span");
 outerSpan.setAttribute("class", "ds");
+
 innerSpan.appendChild(sBtn);
 outerSpan.appendChild(innerSpan);
-setTimeout(document.getElementById("lst-ib").addEventListener("keyup",addButton),3000);
+
+document.getElementById("lst-ib").addEventListener("keypress",addButton);
