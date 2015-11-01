f54bbb91808c34ee248563329ad7b127e5a13a6a
aleung013 <aleung013@gmail.com>
2015-11-01 01:16:36 -0500

    Add button click callback

--- js/content.js
+++ js/content.js
@@ -1,7 +1,17 @@
+
+
+var runSentimentSearch = function(){
+    console.log("Do sentiment Stuff");
+}
+
+
 //Creating Elements
 var btn = document.createElement("input")
 btn.setAttribute("value", "I'm Feeling Happy");
 btn.setAttribute("aria-label", "I'm Feeling Happy");
 btn.setAttribute("type", "submit");
+btn.addEventListener("click", runSentimentSearch);
 //Appending to DOM 
 document.getElementsByTagName("center")[0].appendChild(btn);
+
+
